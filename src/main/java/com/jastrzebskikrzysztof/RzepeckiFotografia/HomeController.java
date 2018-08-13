89c13a4923b9c44ba8e9b9e58609f4a528cde7c4
krzyjast1 <krzyjast1@gmail.com>
2018-08-13 21:11:53 +0200

    Commit #3
    Verifying tokens done in 80%

--- src/main/java/com/jastrzebskikrzysztof/RzepeckiFotografia/HomeController.java
+++ src/main/java/com/jastrzebskikrzysztof/RzepeckiFotografia/HomeController.java
@@ -1,12 +1,31 @@
+/**
+ * @author Krzysztof Jastrzêbski
+**/
+
 package com.jastrzebskikrzysztof.RzepeckiFotografia;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+		
+	}
 	
 	@RequestMapping("/")
 	public String showHomePage() {
@@ -22,11 +41,15 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/processForm")
-	public String processForm(@ModelAttribute("token") Token token) {
+	public String processForm(@Valid @ModelAttribute("token") Token token, BindingResult bindingResult) {
 		
-		//code to validate token 
+		System.out.println(bindingResult);
 		
-		return "gallery";
+		if(bindingResult.hasErrors()) {
+			return "get-photo-form";
+		} else {
+			return "gallery";
+		}
 	}
 	
 }
--- src/main/java/com/jastrzebskikrzysztof/RzepeckiFotografia/Token.java
+++ src/main/java/com/jastrzebskikrzysztof/RzepeckiFotografia/Token.java
@@ -1,7 +1,13 @@
 package com.jastrzebskikrzysztof.RzepeckiFotografia;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public class Token {
 	
+	@NotNull(message="is required")
+	@Pattern(regexp="((?:[a-z][a-z]*[0-9]+[a-z0-9]*))", 
+		message="Token dostępu musi składać się z 6 liter na początku i 3 cyfr na końcu")
 	private String token;
 
 	public String getToken() {
